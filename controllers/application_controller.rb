d929d765c5aa29bf3c5e0934f4f7fa76471e3843
Haoran Deng <haoran@hulu.com>
2013-01-27 12:12:47 +0800

    Add ACE editor for editing post body

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < Sinatra::Base
     enable :sessions
     enable :logging
     set :views, "#{$APP_ROOT}/views"
+    set :public_folder, "#{$APP_ROOT}/public"
+
     get '/' do
         'Hello world'
     end
